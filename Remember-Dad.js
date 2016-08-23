25bbd3e0b5aa09e78751cf7cda3939d96650db46
H. Julian Sloman <julix91@gmx.de>
2016-08-23 02:22:07 -0700

    Starting new project

---
+++ Remember-Dad.js
@@ -0,0 +1,12 @@
+
+var vacationMemories = ['mallorca with judith', 'berlin with dad', 'netherlands with dad', 'austria with dad', 'netherlands and maxi', 'canada with chloe and jason'];
+
+rememberingDad = false;
+
+while (rememberingDad === false) {
+    for (var i = 0; i < vacationMemories.length; i++) {
+        if (vacationMemories[i].includes("dad") !== false) {
+            console.log("now i'm thinking of you, dad! us in " + vacationMemories[i]); var rememberingDad = true; break;
+    } else {console.log("nope, not thinking of you. Thinking of " +vacationMemories[i]); i = Math.floor(Math.random()*vacationMemories.length-1);}
+}}
+
